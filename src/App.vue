7b06d88663c3cb36b66b2af62a4de1f344a1f35d
Matius Rimbe <matiusrimbe.ptik@gmail.com>
2026-05-04 19:04:19 +0800

    create restfull API with json-server

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <script setup>
-import { RouterView, RouterLink } from 'vue-router'
-import Navigation from './components/Navigation.vue';
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header class="container">
-    <Navigation />
-  </header>
   <main class="container">
     <RouterView />
   </main>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,11 @@
 <script setup>
 import Pagination from '@/components/Pagination.vue';
 import ProductCard from '@/components/ProductCard.vue';
-import { onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated, ref } from 'vue';
+import { ref } from 'vue';
 
-const page = ref(1)
-
-function nextPage() {
-  page.value++
-}
-
-onBeforeMount(() => {
-  console.log('On Before Mount')
-})
-
-onMounted(() => {
-  console.log('On Mounted')
-})
-
-onBeforeUpdate(() => {
-  console.log('On Before Update')
-})
-
-onUpdated(() => {
-  console.log('On Updated')
-})
-
-onBeforeUnmount(() => {
-  console.log('ON Before Unmount')
-})
-
-onUnmounted(() => {
-  console.log('On Unmounted')
-})
 </script>
 
 <template>
-  {{ page }}
-
-  <button @click="nextPage">Next Page</button>
   <div class="product-grid">
     <ProductCard />
   </div>
